952e96c9a53fefe8d982268dd528c528cad1fc8a
kayniz12 <142418863+kayniz12@users.noreply.github.com>
2023-09-14 09:19:33 -0400

    Add files via upload

---
+++ Ejer1.js
@@ -0,0 +1,21 @@
+for (k=0;k<11;k++){
+
+    rondamin= 5*36;
+    rondaMax=10*36;
+    
+    valorA= (Math.random()*rondamin)+rondamin;
+
+    num=valorA%37;
+
+  
+    console.log("Ronda numero: ",k);
+  
+    console.log("Valor generado:",valorA):
+  
+    console.log("Nro que cayo en la ruleta", num);
+  
+    console.log("-----------------------------------------");
+  
+   
+  
+  }
